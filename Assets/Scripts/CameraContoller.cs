b58cf6f4b1c2322f545569fbf2a31564d4b5255b
NightWing099 <grohotovkirill6@gmail.com>
2024-09-29 11:58:44 +0500

    initialize

---
+++ Assets/Scripts/CameraContoller.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+
+    public Transform target;
+
+    public float distance = -10f;
+
+    public float height = 0f;
+
+ 
+    public float damping = 5.0f;
+
+
+    public float mapX = 100.0f;
+    public float mapY = 100.0f;
+
+
+    private float minX = 0f;
+    private float maxX = 0f;
+    private float minY = 0f;
+    private float maxY = 0f;
+
+    void Start()
+    {
+
+        minX = transform.position.x;
+        minY = transform.position.y;
+
+        maxX = mapX;
+        maxY = mapY;
+    }
+
+    void Update()
+    {
+        Vector3 wantedPosition = target.TransformPoint(0, height, distance);
+
+        wantedPosition.x = (wantedPosition.x < minX) ? minX : wantedPosition.x;
+        wantedPosition.x = (wantedPosition.x > maxX) ? maxX : wantedPosition.x;
+
+        wantedPosition.y = (wantedPosition.y < minY) ? minY : wantedPosition.y;
+        wantedPosition.y = (wantedPosition.y > maxY) ? maxY : wantedPosition.y;
+
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, (Time.deltaTime * damping));
+    }
+}
